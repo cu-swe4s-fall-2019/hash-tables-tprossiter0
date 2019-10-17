2f93f9506bf87cb4b7a2afca7154217411e40337
root <root@Unbound.localdomain>
2019-10-17 14:37:50 -0600

    all tests fail, build fn now

--- test_hash_functions.py
+++ test_hash_functions.py
@@ -5,36 +5,36 @@ import hash_functions as hf
 
 
 class TestHashFunctions(unittest.TestCase):
-    def TestKeyNonStringASCII(self):
+    def testKeyNonString1ASCII(self):
         with self.assertRaises(Exception) as ex:
-            h_ascii(1, 50)
+            hf.h_ascii(1, 50)
         output = str(ex.exception)
         self.assertEqual(output, "Error, Key must be a string")
 
-    def TestKeyNonStringASCII(self):
+    def testKeyNonString2ASCII(self):
         testlist = [1, 2, 3]
         with self.assertRaises(Exception) as ex:
-            h_ascii(testlist, 50)
+            hf.h_ascii(testlist, 50)
         output = str(ex.exception)
         self.assertEqual(output, "Error, Key must be a string")
 
-    def TestNnonInteger(self):
+    def testNnonInteger1(self):
         with self.assertRaises(Exception) as ex:
-            h_ascii("KeyIsString", "this should not be a string")
+            hf.h_ascii("KeyIsString", "this should not be a string")
         output = str(ex.exception)
         self.assertEqual(output, "Error, N must be an integer")
 
-    def TestNnonInteger(self):
+    def testNnonInteger2(self):
         testlist = [1, 2, 3]
         with self.assertRaises(Exception) as ex:
-            h_ascii("KeyIsString", testlist)
+            hf.h_ascii("KeyIsString", testlist)
         output = str(ex.exception)
         self.assertEqual(output, "Error, N must be an integer")
 
-    def TestBothWrongInputsASCII(self):
+    def testBothWrongInputsASCII(self):
         ermsg = "Error, Key must be a string and N must be an integer"
         with self.assertRaises(Exception) as ex:
-            h_ascii(123, "wrong input")
+            hf.h_ascii(123, "wrong input")
         output = str(ex.exception)
         self.assertEqual(output, ermsg)
 
