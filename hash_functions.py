3364573cc533f63c2b60efd372b0d61ae31d515d
root <root@Unbound.localdomain>
2019-10-17 14:45:13 -0600

    function passes arg tests

--- hash_functions.py
+++ hash_functions.py
@@ -1,6 +1,32 @@
 
 def h_ascii(key, N):
+    """
+    Returns hash value based on size of array
+    and sum of ASCII char values in key(string)
+
+    Parameters
+    --------------
+    key: string to hash
+    N: size of hash table
+
+    Returns
+    --------------
+    Hash Value: index of desired element
+    h_ascii(key,N) ----> element corresponding to key
+
+    """
+    if((isinstance(key, str) is False) and (isinstance(N, int) is False)):
+        raise Exception("Error, Key must be a string and N must be an integer")
+    elif(isinstance(N, int) is False):
+        raise Exception("Error, N must be an integer")
+    elif(isinstance(key, str) is False):
+        raise Exception("Error, Key must be a string")
     return None
 
+
 def h_rolling(key, N):
+    """
+
+    """
+
     return None
