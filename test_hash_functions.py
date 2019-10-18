da0e1a0d205b87208dbf4492f71beaf814f2219a
root <root@Unbound.localdomain>
2019-10-17 18:52:22 -0600

    rolling hash tests complete

--- test_hash_functions.py
+++ test_hash_functions.py
@@ -45,6 +45,51 @@ class TestHashFunctions(unittest.TestCase):
         expected_return_val = 4
         self.assertEqual(expected_return_val, hf.h_ascii(testkey, table_size))
 
+    def testRollingKey1(self):
+        with self.assertRaises(Exception) as ex:
+            hf.h_rolling(12, 50)
+        output = str(ex.exception)
+        self.assertEqual(output, "Error, Key must be a string")
+
+    def testRollingKey2(self):
+        testlist = [1, 2, 3]
+        with self.assertRaises(Exception) as ex:
+            hf.h_rolling(testlist, 50)
+        output = str(ex.exception)
+        self.assertEqual(output, "Error, Key must be a string")
+
+    def testRollingN1(self):
+        with self.assertRaises(Exception) as ex:
+            hf.h_rolling("KeyIsString", "this should not be a string")
+        output = str(ex.exception)
+        self.assertEqual(output, "Error, N must be an integer")
+
+    def testRollingN2(self):
+        testlist = [1, 2, 3]
+        with self.assertRaises(Exception) as ex:
+            hf.h_rolling("KeyIsString", testlist)
+        output = str(ex.exception)
+        self.assertEqual(output, "Error, N must be an integer")
+
+    def testBothWrongInputsRolling(self):
+        ermsg = "Error, Key must be a string and N must be an integer"
+        with self.assertRaises(Exception) as ex:
+            hf.h_rolling(123, "wrong input")
+        output = str(ex.exception)
+        self.assertEqual(output, ermsg)
+
+    def testRollingHashOutput(self):
+        testkey = "abc"
+        akI = [97, 98, 99]
+        p = 53
+        m = 2**64
+        n = 100
+        sum_test = 0
+        for i in range(0, 3):
+            sum_test += akI[i] * p**i
+        sum_test = sum_test % m
+        self.assertEqual(sum_test % n, hf.h_rolling(testkey, n))
+
 
 if __name__ == '__main__':
     unittest.main()
